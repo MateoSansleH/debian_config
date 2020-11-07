1039e444e94658eeb9c6e5b8efd3886dcb8c1943
MateoSansleH <73388681+MateoSansleH@users.noreply.github.com>
2020-11-07 17:50:56 +0100

    Update debian_config.sh

--- debian_config.sh
+++ debian_config.sh
@@ -3,27 +3,22 @@
 utilisateur = administrateur
 
 install_all_needed_package(){
-	apt-get update && apt-get upgrade
-	apt-get install screen
-	apt-get install nano
-	apt-get install vim
+        apt-get update && apt-get upgrade
+        apt-get install screen nano vim tree sudo
 }
 
 delete_cdrom_sources(){
-	sed -i 's/^deb cdrom.*/# deb cdrom/g' /etc/apt/sources.liste
+        sed -i 's/^deb cdrom.*/# deb cdrom/g' /etc/apt/sources.list
 }
 
-add_sudoUSR{
-	usermod -g sudo $utilisateur
+add_sudoUSR(){
+        usermod -g sudo $utilisateur
 }
 
 main(){
-	delete_cdrom_sources
-	install_all_needed_package
-	add_sudoUSR
-}
+        delete_cdrom_sources
+        install_all_needed_package
+        add_sudoUSR
+}                            
 
-main
-
-
-
+main 
